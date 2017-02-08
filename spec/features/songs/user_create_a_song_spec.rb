f9eac1a4ff1ca7481b0bd1127d677b760df161f8
Casey Macaulay <caseyannemacaulay@gmail.com>
2017-02-07 20:58:49 -0700

    adds first two user stories of optional iteration

--- spec/features/songs/user_create_a_song_spec.rb
+++ spec/features/songs/user_create_a_song_spec.rb
@@ -14,4 +14,23 @@ RSpec.feature "User adds a new song" do
     expect(page).to have_content song_title
     expect(page).to have_link "<<Back To Artist Page", href: artist_path(artist)
   end
+
+  context "the submitted data is invalid without a title" do
+    scenario "they see an error message" do
+      artist = create(:artist)
+
+      visit artist_path(artist)
+      click_on "New song"
+      click_on "Create Song"
+
+      expect(page).to have_content "Title can't be blank"
+    end
+  end
 end
+
+# As a user
+# Given that an artist exists in the database
+# When I visit the individual artist page
+# And I click "New song"
+# And I click "Create Song"
+# Then I should see "Title cannot be blank" on the page
