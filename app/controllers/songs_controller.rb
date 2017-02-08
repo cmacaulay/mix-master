5be10766a7acfd8d400c29fee6412e7ec57d0a61
Casey Macaulay <caseyannemacaulay@gmail.com>
2017-02-07 20:04:07 -0700

    adds song functionality

---
+++ app/controllers/songs_controller.rb
@@ -0,0 +1,31 @@
+class SongsController < ApplicationController
+
+  def index
+    @songs = Song.all
+  end
+
+  def show
+    @song = Song.find(params[:id])
+  end
+
+  def new
+    @artist = Artist.find(params[:artist_id])
+    @song = @artist.songs.new
+  end
+
+  def create
+    @artist = Artist.find(params[:artist_id])
+    @song = @artist.songs.new(song_params)
+
+    if @song.save
+      redirect_to @song
+    else
+      render :new
+    end
+  end
+
+  private
+  def song_params
+    params.require(:song).permit(:title)
+  end
+end
